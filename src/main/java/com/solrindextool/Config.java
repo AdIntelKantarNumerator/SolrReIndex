87543909c9f48605e342a2d61ebb044f8802e84f
satyamk54 <satyam.kaushik@kantar.com>
2023-05-30 14:48:58 +0530

    transfer-collections

---
+++ src/main/java/com/solrindextool/Config.java
@@ -0,0 +1,14 @@
+package com.solrindextool;
+
+public class Config {
+    boolean custom;
+    boolean collectionRunner;
+    String customQuery;
+    String sourcePort;
+    String destinationPort;
+    int batchSize;
+    int threadCount;
+    String startTimestamp;
+    String endTimestamp;
+    String[] collections;
+}
---
+++ src/main/java/com/solrindextool/Start.java
@@ -0,0 +1,53 @@
+package com.solrindextool;
+
+import com.google.gson.Gson;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+public class Start {
+    static boolean custom;
+    static String customQuery;
+    static String sourcePort;
+    static String destinationPort;
+    static int batchSize;
+    static int threadCount;
+    static String startTime;
+    static String endTime;
+    static boolean collectionRunner;
+    static String[] collections;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Gson gson = new Gson();
+        Config config;
+
+        try {
+            config = gson.fromJson(new FileReader("config.json"), Config.class);
+            custom = config.custom;
+            collectionRunner = config.collectionRunner;
+            customQuery=config.customQuery;
+            sourcePort= config.sourcePort;
+            destinationPort= config.destinationPort;
+            batchSize=config.batchSize;
+            threadCount=config.threadCount;
+            startTime = config.startTimestamp;
+            endTime = config.endTimestamp;
+            collections = config.collections;
+        } catch (Exception e){
+            System.out.println("config file not found");
+            throw e;
+        }
+
+        Runner runner;
+        if (custom){
+            runner = new Runner(sourcePort, destinationPort, batchSize, threadCount, true, customQuery);
+            runner.copy();
+        } else if (collectionRunner) {
+            runner = new Runner(sourcePort,destinationPort,batchSize,threadCount,collections,startTime,endTime);
+            runner.transferData();
+        } else {
+            runner = new Runner(sourcePort, destinationPort, batchSize, threadCount, startTime, endTime, false);
+            runner.copy();
+        }
+    }
+}
